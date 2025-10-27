ad6b2f046a7fa5fa94b544f48c46ec217cec4ce9
emanuel.ferreira <emanuel2005batista@gmail.com>
2025-10-27 12:47:50 -0300

    ajusta cors

--- app.py
+++ app.py
@@ -13,15 +13,7 @@ from resourses.Carrinho import ns as ns_carrinho
 
 app = Flask(__name__)
 
-
-CORS(
-    app,
-    resources={r"/*": {"origins": ["https://bebel132.github.io"]}},
-    # supports_credentials=True,  # só se você usa cookies/sessions; veja nota abaixo
-    methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"],
-    expose_headers=["Content-Type", "Authorization"]
-)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
